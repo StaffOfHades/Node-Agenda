717b2c6533be31c202ee001be6d0bfe63d9f6aba
Mauricio <mau.graci@gmail.com>
2017-11-25 17:56:58 -0600

    Moved from rendering views to serving them as static files

--- index.js
+++ index.js
@@ -2,21 +2,10 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
-app.set("views", path.join(__dirname, "views"));
-app.engine("html", require("ejs").renderFile);
-app.set("view engine", "html");
+app.use("/", express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-   res.render("registrarUsuario");
-});
-app.get("/horario.html", function (req, res) {
-   res.render("horario");
-});
-app.get("/agregarActividad.html", function (req, res) {
-   res.render("agregarActividad");
-});
-app.get("/registrarUsuario.html", function (req, res) {
-   res.redirect("/");
+   res.redirect("/registrarUsuario.html");
 });
 
 app.listen(3000);
